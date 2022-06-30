947f9bcd86db29b349e00faaeb29cd2bb7a72940
gladyswambura <gladyswahito7@gmail.com>
2022-06-30 22:39:44 +0300

    ft:creates the English dictionary

--- assets/index.js
+++ assets/index.js
@@ -14,5 +14,47 @@ function fetchApi(word) {
   let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
   fetch(url)
     .then((res) => res.json())
-    .then((result) => data(result, word));
+    .then((result) => myWords(result, word));
+}
+
+searchInput.addEventListener("keyup", (event) => {
+  // console.log(event)
+  // checks if the handler event equals enter and outputs in the console
+  if (event.key === "Enter" && event.target.value) {
+    fetchApi(event.target.value)
+  }
+  // console.log(event.target.value)
+
+})
+
+
+removeIcon.addEventListener("click", () => {
+  searchInput.value = "";
+  searchInput.focus();
+  wrapper.classList.remove("active");
+  // infoText.style.color("black");
+  infoText.innerHTML = `Type any existing word and press enter to get meaning, example,
+  synonyms, etc.`
+})
+
+const myWords = (result, word) => {
+  // console.log(result)
+  if (result.title) {
+    infoText.innerHTML = `ouch! The word :${word}&nbsp cannot be found here`;
+  } else {
+    wrapper.classList.add("active");
+
+    let definitions = result[0].meanings[0].definitions[0];
+    console.log(definitions)
+    phonetics = `commonly pronounced as: ${result[0].phonetics[0].text}`
+    // synonyms = `${result[0].synonyms}`;
+
+    document.querySelector(".word p").innerText = result[0].word;
+    document.querySelector(".word span").innerText = phonetics;
+    document.querySelector(".meaning span").innerText = definitions.definition;
+    document.querySelector(".example span").innerText = definitions.example ?? 'No examples here';
+    document.querySelector(".synonym span").innerText= synonyms.synonyms;
+    document.querySelector(".synonym span").innerText = definitions.synonyms ?? 'No synonyms here';
+    // document.querySelector(".synonym span").innerText = synonyms ?? `No synonym for ${word}`
+  }
 }
